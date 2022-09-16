9fc1b00aab21e3f8b59470c4a44084caacb35334
John Couto <90006181+John-Couto@users.noreply.github.com>
2022-09-16 17:58:17 -0300

    Add files via upload

---
+++ Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionário
+{
+    public class Funcionario 
+    {
+        public String ?nome; 
+        public double valorPorHora;
+        public int horas;
+
+        public double total()
+        {
+            return valorPorHora * horas;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Funcionário;
+
+Funcionario funcionario = new Funcionario();
+Console.WriteLine("Digite o nome do funcionário: ");
+funcionario.nome = Console.ReadLine();
+Console.WriteLine("Digite o valor por hora do funcionário: "); 
+funcionario.valorPorHora = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite quantas horas o funcionário trabalhou no mês: ");
+funcionario.horas = Convert.ToInt16(Console.ReadLine());
+
+double total = funcionario.total();
+Console.WriteLine($"O funcionário {funcionario.nome}, que trabalhou {funcionario.horas} horas no mês deve receber " + total);
